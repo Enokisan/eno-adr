c66d8acfd6944bd5677fd940bc07dfc6c106a8ba
Enokisan <eno.eno.business@gmail.com>
2025-04-26 11:25:36 +0900

    ADR全部返す処理追加

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import getAdrList from '../scripts/getAdrList'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -8,21 +9,28 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'ADR List', link: '/pages/adr/' },
+      { text: 'Tag Search', link: '/pages/tag/' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'ADRを探す',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'すべてのADR', link: '/pages/adr/' },
+          { text: 'タグで探す', link: '/pages/tag/' }
+        ],
+      },
+      {
+        text: 'ADR一覧',
+        items: [
+          ...getAdrList()
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/Enokisan/eno-adr' }
     ]
   }
 })
